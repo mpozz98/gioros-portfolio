1e0ba9aaa265accfba17bc6b264a7c9b77fe4ae6
mpozz98 <mpotzaras1234@gmail.com>
2025-02-19 11:52:52 +0200

    Add files via upload
    DATA CLEANING

--- world_life.sql
+++ world_life.sql
@@ -1,81 +1,138 @@
-#EDA 
+#DATA CLEANING
+
+SELECT * FROM world_life_expectancy;
+
+#REMOVING DUPLICATES
+
+SELECT country, year ,COUNT(concat(Country, Year)) 
+from world_life_expectancy
+GROUP BY country, Year
+HAVING COUNT(concat(Country, Year)) > 1
+;
+
+SELECT *
+FROM
+(
+SELECT Row_ID,
+concat(Country, Year),
+ROW_NUMBER() OVER(PARTITION BY concat(Country, Year) ORDER BY concat(Country, Year)) as Row_Num 
+FROM world_life_expectancy
+) AS row_table
+WHERE Row_num > 1
+;
+
+
+DELETE FROM world_life_expectancy
+WHERE ROW_ID IN(
+SELECT ROW_ID
+FROM
+	(
+	SELECT Row_ID,
+	concat(Country, Year),
+	ROW_NUMBER() OVER(PARTITION BY concat(Country, Year) ORDER BY concat(Country, Year)) as Row_Num 
+	FROM world_life_expectancy
+  	) AS row_table
+WHERE Row_num > 1)
+;
+
+--
+#FILLING BLANKS
+
+SELECT *
+FROM world_life_expectancy
+WHERE status = ''
+;
+
+
+SELECT DISTINCT(Status)
+FROM world_life_expectancy
+WHERE status <> '';
+
+SELECT DISTINCT(country)
+FROM world_life_expectancy
+WHERE status  = 'Developing';
+
+UPDATE world_life_expectancy t1
+JOIN world_life_expectancy t2
+ON t1.Country = t2.country
+SET t1.status = 'Developing'
+WHERE t1.status = ''
+AND t2.status <> ''
+AND t2.status = 'Developing'
+;
+
+
+UPDATE world_life_expectancy t1
+JOIN world_life_expectancy t2
+ON t1.Country = t2.country
+SET t1.status = 'Developed'
+WHERE t1.status = ''
+AND t2.status <> ''
+AND t2.status = 'Developed'
+;
+
+--
+
+SELECT *
+FROM world_life_expectancy
+WHERE `Life expectancy` = '';
+
+
+SELECT country, year,`Life expectancy`
+FROM world_life_expectancy
+#WHERE `Life expectancy` = ''
+;
+
+SELECT t1.country, t1.year,t1.`Life expectancy`,
+t2.country, t2.year,t2.`Life expectancy`,
+t3.country, t3.year,t3.`Life expectancy`,
+ROUND((t2.`Life expectancy` + t3.`Life expectancy`)/2, 1)
+FROM world_life_expectancy t1
+JOIN world_life_expectancy t2
+	ON t1.Country = t2.Country
+	AND t1.Year = t2.Year -1
+JOIN world_life_expectancy t3
+	ON t1.Country = t3.Country
+	AND t1.Year = t3.Year + 1
+WHERE t1.`Life expectancy` = ''
+;
+
+UPDATE world_life_expectancy t1
+JOIN world_life_expectancy t2
+	ON t1.Country = t2.Country
+	AND t1.Year = t2.Year -1
+JOIN world_life_expectancy t3
+	ON t1.Country = t3.Country
+	AND t1.Year = t3.Year + 1
+SET t1.`Life expectancy`  = ROUND((t2.`Life expectancy` + t3.`Life expectancy`)/2, 1)
+WHERE t1.`Life expectancy` = ''
+;
+
+--
 
 SELECT *
 FROM world_life_expectancy;
 
-#LIFE INCREASE FOR EACH COUNTRY THE PAST 15 YEARS
 
-SELECT Country, 
-Min(`Life expectancy`), MAX(`Life expectancy`),
-ROUND(Max(`Life expectancy`)- Min(`Life expectancy`), 1) AS Life_increase
-FROM world_life_expectancy
-GROUP BY Country
-HAVING Min(`Life expectancy`) <> 0
-ORDER BY 4 DESC
-;
 
-# AVERAGE LIFE EXPECTANCY BY YEAR
 
-SELECT year, 
-ROUND(AVG(`Life expectancy`), 1)
-FROM world_life_expectancy
-WHERE `Life expectancy` <> 0
-GROUP BY Year
-ORDER BY 2;
 
---
 
-# LIFE EXPECTANCY AND GDP CORRELATION
 
-SELECT Country, 
-ROUND(AVG(`Life expectancy`),1) AS Life_exp, 
-ROUND(AVG(GDP),1) AS GDP
-FROM world_life_expectancy
-GROUP BY country
-HAVING Life_exp > 0
-AND GDP > 0
-ORDER BY 2
-;
 
-SELECT 
-SUM(CASE WHEN GDP >= 1500 THEN 1 ELSE 0 END) High_GDP_Count,
-ROUND(AVG(CASE WHEN GDP >= 1500 THEN `Life expectancy` ELSE NULL END) , 2) Low_GDP_Life_expectancy,
-SUM(CASE WHEN GDP <= 1500 THEN 1 ELSE 0 END) High_GDP_Count,
-ROUND(AVG(CASE WHEN GDP <= 1500 THEN `Life expectancy` ELSE NULL END) , 2) Low_GDP_Life_expectancy
-FROM world_life_expectancy
-;
 
---
 
-SELECT Status, 
-ROUND(AVG(`Life expectancy`),1),
-COUNT(DISTINCT Country)
-FROM world_life_expectancy
-GROUP BY Status
-;
 
---
-#LIFE EXPECTANCY AND BMI CORRELATION
 
-SELECT Country, 
-ROUND(AVG(`Life expectancy`),1) AS Life_exp, 
-ROUND(AVG(BMI),1) AS BMI
-FROM world_life_expectancy
-GROUP BY country
-HAVING Life_exp > 0
-AND BMI > 0
-ORDER BY BMI DESC
-;
 
---
-#ROLLING ADULT MORTALITY COUNTS BY YEAR (UNFORTONATELY NO TOTAL POPULATION DATA)
 
-SELECT Country, Year, 
-`Life expectancy`,
-`Adult Mortality`,
-SUM(`Adult Mortality`) OVER (PARTITION BY Country ORDER BY Year) AS Rolling_Total
-FROM world_life_expectancy
-;
+
+
+
+
+
+
+
 
 
 
